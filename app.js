a048222e94f6a3b8723b04a50cce2e105e125d7f
Sam Jones <sam.jones@spadio.org>
2018-01-10 17:17:28 +0000

    Signed-off-by: Sam Jones <sam.jones@spadio.org>

--- app.js
+++ app.js
@@ -11,22 +11,32 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var list = [];
 var rounds = {sam: "1", max: "1"};
 
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+function generateNew() {
+	console.log("Generating new...");
+	pastebin.getPaste('ZcV7pqsR').then(function(paste) {
+		list = shuffle(paste.split("\n"));
+		data = {};
+		data.challenge1 = list[0];
+		data.challenge2 = list[1];
+		io.emit('result', data);
+		rounds = {sam:randomInt(1, 4).toString(), max:randomInt(1, 4).toString()};
+		io.emit("roundChange", rounds);
+	}).fail(function(err) {
+		console.log("Error caught!\n" + err);
+	});
+}
+
 function socketSetup(socket) {
-	socket.emit("roundChange", {sam:rounds.sam, max:rounds.max});
+	//socket.emit("roundChange", {sam:rounds.sam, max:rounds.max});
 
 	socket.on('generateNew', function() {
-		console.log("Generating new...");
-		pastebin.getPaste('ZcV7pqsR').then(function(paste) {
-			list = shuffle(paste.split("\n"));
-			data = {};
-			data.challenge1 = list[0];
-			data.challenge2 = list[1];
-			io.emit('result', data);
-		}).fail(function(err) {
-			console.log("Error caught!");
-		});
+		generateNew();
 	});
-	socket.on("updateRound", (data) => { rounds = data; io.emit("roundChange", {sam:rounds.sam, max:rounds.max}); });
+	//socket.on("updateRound", (data) => { rounds = data; io.emit("roundChange", {sam:rounds.sam, max:rounds.max}); });
 }
 
 function shuffle(array) {
@@ -59,10 +69,6 @@ io.on('connection', function(socket) {
 	if (list.length == 0) {
 		pastebin.getPaste('ZcV7pqsR').then(function(paste) {
 			list = shuffle(paste.split("\n"));
-			data = {};
-			data.challenge1 = list[0];
-			data.challenge2 = list[1];
-			socket.emit('result', data);
 			socketSetup(socket);
 		}).fail(function(err) {
 			console.log("Error caught!");
@@ -73,6 +79,7 @@ io.on('connection', function(socket) {
 		data.challenge1 = list[0];
 		data.challenge2 = list[1];
 		socket.emit('result', data);
+		socket.emit("roundChange", rounds);
 		socketSetup(socket);
 	}
 });
