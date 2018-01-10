47b2ee70d8230ddbb629d39545d0e4745997c9db
Sam Jones <sam.jones@spadio.org>
2018-01-10 15:48:58 +0000

    Signed-off-by: Sam Jones <sam.jones@spadio.org>

--- app.js
+++ app.js
@@ -6,38 +6,14 @@ var io = require('socket.io').listen(server);
 var PastebinAPI = require('pastebin-js'),
     pastebin = new PastebinAPI('3cc5c6cbd8d898bcce52a24796713011');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-var list = ["ERROR", "ERROR"]
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var list = [];
+var rounds = {sam: "1", max: "1"};
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+function socketSetup(socket) {
+	socket.emit("roundChange", {sam:rounds.sam, max:rounds.max});
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/files/index.html');
-});
-
-io.on('connection', function(socket) {
-	data = {};
-	data.challenge1 = list[0];
-	data.challenge2 = list[1];
-	socket.emit('result', data);
 	socket.on('generateNew', function() {
 		console.log("Generating new...");
 		pastebin.getPaste('ZcV7pqsR').then(function(paste) {
@@ -50,6 +26,55 @@ io.on('connection', function(socket) {
 			console.log("Error caught!");
 		});
 	});
+	socket.on("updateRound", (data) => { rounds = data; io.emit("roundChange", {sam:rounds.sam, max:rounds.max}); });
+}
+
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+app.get("/index.css", (req, res) => {
+	res.sendFile(__dirname + "/files/index.css");
+});
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/files/index.html');
+});
+
+io.on('connection', function(socket) {
+	if (list.length == 0) {
+		pastebin.getPaste('ZcV7pqsR').then(function(paste) {
+			list = shuffle(paste.split("\n"));
+			data = {};
+			data.challenge1 = list[0];
+			data.challenge2 = list[1];
+			socket.emit('result', data);
+			socketSetup(socket);
+		}).fail(function(err) {
+			console.log("Error caught!");
+			socketSetup(socket);
+		});
+	} else {
+		data = {};
+		data.challenge1 = list[0];
+		data.challenge2 = list[1];
+		socket.emit('result', data);
+		socketSetup(socket);
+	}
 });
 
 server.listen(server_port, () => {
